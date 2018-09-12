b5a1a366a036b93b3ab84c5f50be4db57233c6ec
Liu Xiangrong <xiangliu@TP.EDU.SG>
2018-09-12 04:42:10 +0000

    testing

---
+++ FunctionApp5/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp3
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+    }
+
+    public class User
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ FunctionApp5/ProductRating.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp3
+{
+    public class ProductRating
+    {
+        public string ProductId { get; set; }
+        public string UserId { get; set; }
+        public string LocationName { get; set; }
+        public int Rating { get; set; }
+        public string UserNotes { get; set; }
+        public string id { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public void PopulateIdandTime()
+        {
+            id = Guid.NewGuid().ToString();
+            Timestamp = DateTime.UtcNow;
+        }
+    }
+}
